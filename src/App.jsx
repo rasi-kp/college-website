302fe3200e9ba7aaeb98c4f26ae76af4dacd4de6
unknown <crisbinjoseph.dev@gmail.com>
2024-07-26 23:28:45 +0530

    modifiedapps

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <h1>hello</h1>
+        <h1>helo</h1>
       </div>
       <h1>Vite + React</h1>
       <button>heyy</button>
