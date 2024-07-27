aa7a1c5d1308e3f4a1d5922fbc77728f24dbc2d4
Rasi K P <rasir239@gmail.com>
2024-07-28 00:07:47 +0530

    implement tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './index.css'; // Ensure this import is here
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="max-w-screen-xl h-screen mx-auto p-8 text-center bg-slate-400">
+      <div className="flex justify-center items-center space-x-8">
+        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
+          <img src={viteLogo} className="h-24 p-6 transition-filter duration-300 will-change-transform hover:drop-shadow-lg" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+          <img src={reactLogo} className="h-24 p-6 transition-filter duration-300 will-change-transform hover:drop-shadow-lg" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <h4 className='btn danger text-cyan-600'>click me</h4>
+      <h1 className=" text-4xl font-bold ">Vite + React</h1>
+      <h1 className=" text-2xl font-bold underline">Hello world!</h1>
+      <div className="p-8">
+        <h4 className="text-cyan-600 p-2">click me</h4>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-gray-500">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
